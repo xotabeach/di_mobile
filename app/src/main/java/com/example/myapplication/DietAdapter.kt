edf87ce326b536fa0c5faee7748bef075814a422
Xotabeach <xotabich1941@gmail.com>
2024-06-16 21:30:36 +0300

    already

--- app/src/main/java/com/example/myapplication/DietAdapter.kt
+++ app/src/main/java/com/example/myapplication/DietAdapter.kt
@@ -5,7 +5,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.myapplication.R
 
 data class DietDay(
     val day: String,
